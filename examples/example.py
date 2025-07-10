35466412a17ce1d534d8c067b5fa60e2705070b7
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-07-10 15:51:59 -0400

    improving readme

--- examples/example.py
+++ examples/example.py
@@ -1,5 +1,5 @@
 import pygame
-from gamepad_mapper import load_or_map
+from gamepad_mapper import load_or_map, read_gamepad
 
 pygame.init()
 pygame.joystick.init()
@@ -8,3 +8,6 @@ assert pygame.joystick.get_count() > 0, "No game controller found."
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 mapping = load_or_map(joystick, ["Roll", "Pitch", "Throttle", "Yaw"], ["arm"], force=True, name="example")
+axes, buttons = read_gamepad(joystick, mapping)
+print(axes)
+print(buttons)
